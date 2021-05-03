789963800214e9939235a49fcdbfdb7a17df4579
Reinhardt <73813638+NotMarx@users.noreply.github.com>
2021-05-03 11:35:36 +0700

    Export classes
    Change Logs:
    Export class:
    - [`ClientUser`](https://github.com/reloadedjs/reloaded/blob/dev/src/Structures/ClientUser.js)
    - [`Message`](https://github.com/reloadedjs/reloaded/blob/dev/src/Structures/Message.js)
    - Much More 😳

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 module.exports = {
     Client: require("./src/Client"),
+    ClientUser: require("./src/Structures/ClientUser"),
     Constants: require("./src/Constants"),
     Collection: require("./src/Structures/Collection"),
-    Guild: require("./src/Structures/Guild"),
+    Message: require("./src/Structures/Message"),
     MessageEmbed: require("./src/Structures/MessageEmbed"),
+    Payload: require("./src/WebSocket/Payload"),
+    Presence: require("./src/Structures/Presence"),
+    RestManager: require("./src/Rest/RestManager"),
+    User: require("./src/Structures/User"),
+    WebsocketManager: require("./src/WebSocket/Websocket"),
     version: require("./package.json").version
 };
