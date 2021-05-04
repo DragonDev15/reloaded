421ff445ea29d1669d1902d21fad9064335dbe20
Reinhardt <73813638+NotMarx@users.noreply.github.com>
2021-05-04 08:36:42 +0700

    Update export path

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 module.exports = {
-    Client: require("./libs/Client"),
-    Constants: require("./libs/Constants"),
-    Collection: require("./libs/Structures/Collection"),
-    MessageEmbed: require("./libs/Structures/MessageEmbed"),
+    Client: require("./lib/Client"),
+    ClientUser: require("./lib/Structures/ClientUser"),
+    Constants: require("./lib/Constants"),
+    Collection: require("./lib/Structures/Collection"),
+    Message: require("./lib/Structures/Message"),
+    MessageEmbed: require("./lib/Structures/MessageEmbed"),
+    Payload: require("./lib/WebSocket/Payload"),
+    Presence: require("./lib/Structures/Presence"),
+    RestManager: require("./lib/Rest/RestManager"),
+    User: require("./lib/Structures/User"),
+    WebsocketManager: require("./lib/WebSocket/Websocket"),
     version: require("./package.json").version
 };
